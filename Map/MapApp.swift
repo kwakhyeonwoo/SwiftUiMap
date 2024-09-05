6cd81cf5c873f3a4115a4c3243232eda63c5b8ba
곽현우 <polla723@naver.com>
2024-09-05 13:49:06 +0900

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by 곽현우 on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
